5279c6887e7b9b39796d3ee7e3e3e069f74d3c25
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-01 19:40:09 -0700

    Server running and receive GET request for static HTML

--- server/index.js
+++ server/index.js
@@ -4,8 +4,17 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('../public'));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/restaurant/:restaurantID/photos', (req, res) => {
+  const restaurantID = parseInt(req.params, 10);
+  if (typeof restaurantID !== 'number') {
+    res.status(400).send('Bad input, must be a valid ID number.');
+  } else {
+    res.status(200).send('OK');
+  }
+});
 
 const port = process.env.PORT || 3004;
 app.listen(port, console.log(`Listening on ${port}`));
